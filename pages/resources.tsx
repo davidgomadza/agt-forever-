1e542f234de3d9aadcdcf35130d3b8a7ce1cf5a7
david gomadza <49430339+davidgomadza@users.noreply.github.com>
2025-10-21 17:35:49 +0100

    Create resources.tsx

---
+++ pages/resources.tsx
@@ -0,0 +1,15 @@
+export default function Resources() {
+  return (
+    <div>
+      <h1>AGT Resources</h1>
+      <ul>
+        <li><a href="https://github.com/davidgomadza/AGT-Forever">AGT-Forever: Next.js starter for forever tech</a></li>
+        <li><a href="https://github.com/davidgomadza/AGT-Platform">AGT-Platform: Payment platform code</a></li>
+        <li><a href="https://github.com/davidgomadza/84">84: 84 subsets for AGT cures</a></li>
+        <li><a href="https://github.com/davidgomadza/AGTagt">AGTagt: AGT core</a></li>
+        <li><a href="https://github.com/davidgomadza/studio">Studio: Development studio</a></li>
+      </ul>
+      {/* Embed code viewers if needed */}
+    </div>
+  );
+}
